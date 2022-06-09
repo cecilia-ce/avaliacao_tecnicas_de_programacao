116311f5a0a7a4d5f381cdcb628d0b56ee63a92d
Weslley Gabriel <49175123+WeslleyGRR@users.noreply.github.com>
2022-06-09 19:54:21 -0300

    Update MysqlConnect.java

--- src/DAO/MysqlConnect.java
+++ src/DAO/MysqlConnect.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class MysqlConnect {
     private static final String URL = "jdbc:mysql://localhost:3306/DBCADV";
-    private static final String USER = "rood";
+    private static final String USER = "root";
     private static final String PASS = "Admin";
     private static final String DRIVER = "com.mysql.jdbc.Driver";
 
